4f6ce49453c7cacc5df59544d445d7184bc378bb
kot-shrodingera <kot.shrodingera@gmail.com>
2020-08-05 08:22:38 +0600

    Добавлены пути в конфиг

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,10 @@
 import typescript from '@rollup/plugin-typescript';
 
 export default {
+  input: './src/index.ts',
   output: {
     format: 'es',
+    file: `./dist/index.js`,
   },
   plugins: [typescript()],
 };
